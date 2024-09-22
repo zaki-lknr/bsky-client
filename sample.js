bed1e7e86c96822a57ddaa1e9c7c0b43cc878dcd
zaki-lknr <zaki.hmkc+github@gmail.com>
2024-09-22 19:35:59 +0900

    add: migrate from html-sample
    <https://github.com/zaki-lknr/html-js-sample/blob/main/sample/bsky-client.js>

---
+++ sample.js
@@ -0,0 +1,49 @@
+import {JpzBskyClient} from "./bsky-client.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById('btn_save').addEventListener('click', ()=> {
+        save_configure();
+    });
+    document.getElementById('btn_load').addEventListener('click', ()=> {
+        load_configure();
+    });
+    document.getElementById('btn_post').addEventListener('click', ()=> {
+        post();
+    });
+
+});
+
+
+const save_configure = () => {
+    const bsky_id = document.getElementById("bsky_id").value;
+    const bsky_pass = document.getElementById("bsky_pass").value;
+
+    const configuration = {
+        bsky_id: bsky_id,
+        bsky_pass: bsky_pass
+    }
+    localStorage.setItem('bsky_configuration', JSON.stringify(configuration));
+}
+
+const load_configure = () => {
+    const configure = JSON.parse(localStorage.getItem('bsky_configuration'));
+
+    document.getElementById("bsky_id").value = configure.bsky_id;
+    document.getElementById("bsky_pass").value = configure.bsky_pass;
+
+    return configure;
+}
+
+const post = async () => {
+    // console.log("start")
+    const configure = load_configure();
+    const message = document.getElementById("post_string").value;
+    const local_images = document.getElementById("file").files;
+
+    const bsky = new JpzBskyClient(configure.bsky_id, configure.bsky_pass);
+    if (local_images.length > 0) {
+        bsky.setImageFiles(local_images);
+    }
+    bsky.post(message);
+}
